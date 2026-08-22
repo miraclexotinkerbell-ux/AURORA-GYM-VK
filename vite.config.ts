052f123c19ddc95d6011cf161e6efc338af104f7
miraclexotinkerbell-ux <miraclexotinkerbell@gmail.com>
2026-08-22 22:09:23 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? './' : '/',
+    base: '/AURORA-GYM-by-VK/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
